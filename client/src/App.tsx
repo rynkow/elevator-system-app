8fea4ae3b79a76681ce6bad042d361179499732d
rynkow <rynkowski.sz@gmail.com>
2022-05-21 00:16:42 +0200

    app loads initial system state form server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Grid from "./component/Grid";
+import {ElevatorSystemState} from "./interface/elevatorSystemState.interface";
+import FetcherService from "./service/fetcherService";
 
 const App = () => {
     const [maxFloor, setMaxFloor] = useState(0);
@@ -11,10 +13,26 @@ const App = () => {
     const [upRequests, setUpRequests] = useState<number[]>([]);
     const [downRequests, setDownRequests] = useState<number[]>([]);
 
+    const setState = (state: ElevatorSystemState) => {
+        setMaxFloor(state.maxFloor);
+        setElevatorCount(state.elevatorCount);
+        setElevatorFloors(state.elevatorFloors);
+        setDownRequests(state.downRequests);
+        setUpRequests(state.upRequests);
+        setElevatorDirections(state.elevatorDirections);
+        setElevatorDestinations(state.elevatorDestinations);
+    }
 
+    useEffect(()=>{
+        const fetchState = async () => {
+            const systemState: ElevatorSystemState | undefined = await FetcherService.systemState();
+            if (systemState !== undefined) setState(systemState);
+        };
+        fetchState().then();
+    },[])
 
-    const gridElements: JSX.Element[][] = Array.from(Array(5).keys()).map((i) =>
-        Array.from(Array(3).keys()).map((j) =>
+    const gridElements: JSX.Element[][] = Array.from(Array(maxFloor).keys()).map((i) =>
+        Array.from(Array(elevatorCount + 1).keys()).map((j) =>
             <div className="element" key={`${i}_${j}`}> {`${i}_${j}`} </div>
         )
     )
