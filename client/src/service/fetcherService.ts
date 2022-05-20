ff7554bcc9e4980955d09106784870de5926c505
rynkow <rynkowski.sz@gmail.com>
2022-05-21 00:04:08 +0200

    fetcher service for making requests to the server

---
+++ client/src/service/fetcherService.ts
@@ -0,0 +1,30 @@
+import {ElevatorSystemState} from "../interface/elevatorSystemState.interface";
+
+class FetcherService{
+    public static systemState = async () => {
+        const request = await fetch("http://localhost:8080/elevators", {method: "GET"});
+        if (!request.ok) return undefined;
+        const systemState: ElevatorSystemState = await request.json()
+        return systemState;
+    }
+
+    public static nextStep = async () => {
+        const request = await fetch("http://localhost:8080/elevators", {method: "PATCH"});
+        if (!request.ok) return undefined;
+        const systemState: ElevatorSystemState = await request.json()
+        return systemState;
+    }
+
+    public static newDestination = async (elevatorId: number, destination: number) => {
+        const request = await fetch(`http://localhost:8080/destination/${elevatorId}/${destination}`, {method: "PUT"});
+        return request.ok;
+    }
+
+    public static newRequest = async (floor: number, direction: number) => {
+        const request = await fetch(`http://localhost:8080//request/${floor}/${direction}"`, {method: "PUT"});
+        return request.ok;
+    }
+
+}
+
+export default FetcherService;
