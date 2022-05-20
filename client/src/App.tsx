236496b64e63c94d69aeb05fd8af3ab77600b016
rynkow <rynkowski.sz@gmail.com>
2022-05-20 23:49:33 +0200

    added states to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Grid from "./component/Grid";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+    const [maxFloor, setMaxFloor] = useState(0);
+    const [elevatorCount, setElevatorCount] = useState(0);
+    const [elevatorFloors, setElevatorFloors] = useState<number[]>([]);
+    const [elevatorDirections, setElevatorDirections] = useState<number[]>([]);
+    const [elevatorDestinations, setElevatorDestinations] = useState<number[][]>(new Array([]));
+    const [upRequests, setUpRequests] = useState<number[]>([]);
+    const [downRequests, setDownRequests] = useState<number[]>([]);
+
+
+
+    const gridElements: JSX.Element[][] = Array.from(Array(5).keys()).map((i) =>
+        Array.from(Array(3).keys()).map((j) =>
+            <div className="element" key={`${i}_${j}`}> {`${i}_${j}`} </div>
+        )
+    )
+
+    return (
+        <div className="App">
+            <Grid elements={gridElements}/>
+        </div>
+    );
 }
+
 
 export default App;
