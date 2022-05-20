60eab1a7304068e319a32a469667996b87349148
rynkow <rynkowski.sz@gmail.com>
2022-05-20 23:55:20 +0200

    system state interface

---
+++ client/src/interface/elevatorSystemState.interface.ts
@@ -0,0 +1,10 @@
+export interface ElevatorSystemState{
+    maxFloor: number,                       // max floor
+    elevatorCount: number,                  // number of elevators in the system
+    upRequests: number[],                   // floor numbers of floors with active up request
+    downRequests: number[],                 // floor numbers of floors with active down request
+    elevatorFloors: number[],               // current floors of the elevators
+    elevatorDirections: number[],           // current directions of the elevators
+    reservedElevators: number[],            // indices of reserved elevators
+    elevatorDestinations: number[][],       // for each elevator there is a list of set destinations
+}
