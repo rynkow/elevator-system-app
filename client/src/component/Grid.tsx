b4255c8972ad40b88ed6e99123e016e8aa410628
rynkow <rynkowski.sz@gmail.com>
2022-05-21 02:35:13 +0200

    shrink last column in grid

--- client/src/component/Grid.tsx
+++ client/src/component/Grid.tsx
@@ -7,7 +7,7 @@ const Grid = (props:{elements: JSX.Element[][]}) => {
             {props.elements.map((row, row_index)=>
                 <div className="grid-row" key={row_index}>
                     {row.map((element, column_index)=>
-                        <div className="grid-cell" key={column_index}>
+                        <div className={`grid-cell ${column_index===row.length-1?"last-column-cell":""}`} key={column_index}>
                             {element}
                         </div>
                     )}
