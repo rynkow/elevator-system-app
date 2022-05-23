55ce8620dcac2221fe9d5589cbd8f31b1da9baef
rynkow <rynkowski.sz@gmail.com>
2022-05-24 00:44:15 +0200

    added placeholders in form

--- client/src/component/ControlBar.tsx
+++ client/src/component/ControlBar.tsx
@@ -2,11 +2,12 @@ import "./ControlBar.css"
 import {useState} from "react";
 
 const ControlBar = (props: { onNextStep: () => void, onNewParameters: (maxFloor: number, elevatorCount: number) => void }) => {
-    const [maxFloor, setMaxFloor] = useState(1)
-    const [elevatorCount, setElevatorCount] = useState(1)
+    const [maxFloor, setMaxFloor] = useState<number>()
+    const [elevatorCount, setElevatorCount] = useState<number>()
     const [incorrectInput, setIncorrectInput] = useState(false);
 
     const onSubmit = () => {
+        if (maxFloor === undefined || elevatorCount === undefined) return;
         let correct = true
         if (maxFloor > 20 || maxFloor < 0) correct = false;
         if (elevatorCount > 16 || elevatorCount < 0) correct = false;
@@ -26,13 +27,13 @@ const ControlBar = (props: { onNextStep: () => void, onNewParameters: (maxFloor:
                     <label>
                         Maximum Floor
                         <br/>
-                        <input type="number" value={maxFloor}
+                        <input placeholder="max 20" type="number" value={maxFloor}
                                onChange={e =>setMaxFloor(parseInt(e.target.value))}/>
                     </label>
                     <label>
                         Number of Elevators
                         <br/>
-                        <input type="number" value={elevatorCount}
+                        <input placeholder="max 16" type="number" value={elevatorCount}
                                onChange={e => setElevatorCount(parseInt(e.target.value))}/>
                     </label>
                     <button className="control-button"
