63a10fe0fd07f59ebcad665aeb7061a1f5cc1ae4
rynkow <rynkowski.sz@gmail.com>
2022-05-24 18:56:39 +0200

    changed undefined to NaN in initial form values

--- client/src/component/ControlBar.tsx
+++ client/src/component/ControlBar.tsx
@@ -2,13 +2,13 @@ import "./ControlBar.css"
 import {useState} from "react";
 
 const ControlBar = (props: { onNextStep: () => void, onNewParameters: (maxFloor: number, elevatorCount: number) => void }) => {
-    const [maxFloor, setMaxFloor] = useState<number>()
-    const [elevatorCount, setElevatorCount] = useState<number>()
+    const [maxFloor, setMaxFloor] = useState<number>(NaN)
+    const [elevatorCount, setElevatorCount] = useState<number>(NaN)
     const [incorrectInput, setIncorrectInput] = useState(false);
 
     const onSubmit = () => {
-        if (maxFloor === undefined || elevatorCount === undefined) return;
         let correct = true
+        if (isNaN(maxFloor) || isNaN(elevatorCount)) correct = false;
         if (maxFloor > 20 || maxFloor < 0) correct = false;
         if (elevatorCount > 16 || elevatorCount < 0) correct = false;
         setIncorrectInput(!correct);
@@ -27,14 +27,14 @@ const ControlBar = (props: { onNextStep: () => void, onNewParameters: (maxFloor:
                     <label>
                         Maximum Floor
                         <br/>
-                        <input placeholder="max 20" type="number" value={maxFloor}
-                               onChange={e =>setMaxFloor(parseInt(e.target.value))}/>
+                        <input placeholder="max 20" type="number" value={isNaN(maxFloor)?"":maxFloor}
+                               onChange={e =>setMaxFloor(parseInt(e.target.value))} max={20} min={1}/>
                     </label>
                     <label>
                         Number of Elevators
                         <br/>
-                        <input placeholder="max 16" type="number" value={elevatorCount}
-                               onChange={e => setElevatorCount(parseInt(e.target.value))}/>
+                        <input placeholder="max 16" type="number" value={isNaN(elevatorCount)?"":elevatorCount}
+                               onChange={e => setElevatorCount(parseInt(e.target.value))} max={20} min={1}/>
                     </label>
                     <button className="control-button"
                             onClick={onSubmit}>Submit
